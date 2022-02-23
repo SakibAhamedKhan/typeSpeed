4bf5fe0126e3ca5cb12feede81d94565c0e2c2cc
SakibAhamedKhan <sakibahamedkhan01@gmail.com>
2022-02-24 01:04:35 +0600

    Typing done

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
 // Task String Text
 let str = [
-	'Therere',
-	'Please take your dog, Cali, out for a walk – he really needs some exercise! What a beautiful day it is on the beach,here in beautiful and sunny Hawaii.',
-	'Rex Quinfrey, a renowned scientist, created plans for an invisibility machine. Do you know why all those chemicals are so hazardous to the environment?',
-	'You never did tell me how many copper pennies where in that jar; how come? Max Joykner sneakily drove his car around every corner looking for his dog.',
-	'The two boys collected twigs outside, for over an hour, in the freezing cold! When do you think they will get back from their adventure in Cairo, Egypt?',
-	'Trixie and Veronica, our two cats, just love to play with their pink ball of yarn. We climbed to the top of the mountain in just under two hours; isn’t that great?',
-	'Hector quizzed Mr. Vexife for two hours, but he was unable to get any information. I have three things to do today: wash my car, call my mother, and feed my dog.',
-	'Xavier Puvre counted eighty large boxes and sixteen small boxes stacked outside. The Reckson family decided to go to an amusement park on Wednesday.',
-	'That herd of bison seems to be moving quickly; does that seem normal to you? All the grandfather clocks in that store were set at exactly 3 o’clock.',
-	'There are so many places to go in Europe for a vacation--Paris, Rome, Prague, etc. Those diamonds and rubies will make a beautiful piece of jewelry.',
-	'The steamboats seemed to float down the Mississippi River at a snail’s pace. In order to keep up at that pace, Zack Squeve would have to work all night.'
+	`Therere's`,
+	`Please take your dog, Cali, out for a walk - he really needs some exercise! What a beautiful day it is on the beach,here in beautiful and sunny Hawaii.`,
+	`Rex Quinfrey, a renowned scientist, created plans for an invisibility machine. Do you know why all those chemicals are so hazardous to the environment?`,
+	`You never did tell me how many copper pennies where in that jar; how come? Max Joykner sneakily drove his car around every corner looking for his dog.`,
+	`The two boys collected twigs outside, for over an hour, in the freezing cold! When do you think they will get back from their adventure in Cairo, Egypt?`,
+	`Trixie and Veronica, our two cats, just love to play with their pink ball of yarn. We climbed to the top of the mountain in just under two hours; isn't that great?`,
+	`Hector quizzed Mr. Vexife for two hours, but he was unable to get any information. I have three things to do today: wash my car, call my mother, and feed my dog.`,
+	`Xavier Puvre counted eighty large boxes and sixteen small boxes stacked outside. The Reckson family decided to go to an amusement park on Wednesday.`,
+	`That herd of bison seems to be moving quickly; does that seem normal to you? All the grandfather clocks in that store were set at exactly 3 o'clock.`,
+	`There are so many places to go in Europe for a vacation-Paris, Rome, Prague, etc. Those diamonds and rubies will make a beautiful piece of jewelry.`,
+	`The steamboats seemed to float down the Mississippi River at a snails pace. In order to keep up at that pace, Zack Squeve would have to work all night.`
 ];
 
 let p=0;
 let len = str[0].length;
+let load = Math.floor(Math.random() * 10);
+// let load=0;
 console.log(len);
-for(const s of str[0]){
+for(const s of str[load]){
 	let container = `<span id="word-${p}" class="word">${s}</span>`;
 	console.log(typeof(container));
 	document.getElementById('text-contain').innerHTML+=(container);
@@ -35,12 +37,30 @@ document.getElementById(cls).classList.add('active');
 document.addEventListener('keydown', function(event){
 	console.log(event.key, position);
 	if(event.key.length==1){
-		if(event.key == str[0][position]){
+		if(event.key == str[load][position]){
+			document.getElementById(`word-${position}`).style.backgroundColor = '#E7FBD3'
+			document.getElementById(`word-${position}`).classList.remove('active');
+			if(position<str[load].length-1){
+				document.getElementById(`word-${position+1}`).classList.add('active');
+			}
+			position++;
+			console.log(position, str[load].length);
+			if(position==str[load].length){
+				document.getElementById('congratulation').innerText =`Congratulation!!! You have Complete the Test.`;
+			}
 			
 		} else{
-
+			document.getElementById(`word-${position}`).style.backgroundColor = '#FFC0CB'
 		}
 	} else if(event.key == 'Backspace'){
-		
-	} 
+		document.getElementById(`word-${position}`).style.backgroundColor = 'white';
+		document.getElementById(`word-${position}`).classList.add('active');
+	}
+	
 })
+
+
+// Try again button
+const tryAgain = () => {
+	window.location.reload();
+}
