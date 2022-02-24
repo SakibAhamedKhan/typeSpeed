251eeb77b13775e9408d7971fa7ea86189fe1d30
SakibAhamedKhan <sakibahamedkhan01@gmail.com>
2022-02-24 23:40:27 +0600

    countdown and Finished text added

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let str = [
 	`Therere's`,
 	`Please take your dog, Cali, out for a walk - he really needs some exercise! What a beautiful day it is on the beach,here in beautiful and sunny Hawaii.`,
 	`Rex Quinfrey, a renowned scientist, created plans for an invisibility machine. Do you know why all those chemicals are so hazardous to the environment?`,
-	`You never did tell me how many copper pennies where in that jar; how come? Max Joykner sneakily drove his car around every corner looking for his dog.`,
+	`You never did tell me how many copper where in that jar; how come? Max Joykner sneakily drove his car around every corner looking for his dog.`,
 	`The two boys collected twigs outside, for over an hour, in the freezing cold! When do you think they will get back from their adventure in Cairo, Egypt?`,
 	`Trixie and Veronica, our two cats, just love to play with their pink ball of yarn. We climbed to the top of the mountain in just under two hours; isn't that great?`,
 	`Hector quizzed Mr. Vexife for two hours, but he was unable to get any information. I have three things to do today: wash my car, call my mother, and feed my dog.`,
@@ -15,8 +15,8 @@ let str = [
 
 let p=0;
 let len = str[0].length;
-let load = Math.floor(Math.random() * 10);
-// let load=0;
+// let load = Math.floor(Math.random() * 10);
+let load=0;
 console.log(len);
 for(const s of str[load]){
 	let container = `<span id="word-${p}" class="word">${s}</span>`;
@@ -34,32 +34,81 @@ for(const s of str[load]){
 let position = 0;
 let cls = `word-${position}`;
 document.getElementById(cls).classList.add('active');
+
+
+let time = 5;
+let timePos=0;
+let timeHas =true;
+let timeFlag = true;
+let timeDecreaseNeed = true;
+document.getElementById('countDown').innerText = time;
 document.addEventListener('keydown', function(event){
-	console.log(event.key, position);
-	if(event.key.length==1){
-		if(event.key == str[load][position]){
-			document.getElementById(`word-${position}`).style.backgroundColor = '#E7FBD3'
-			document.getElementById(`word-${position}`).classList.remove('active');
-			if(position<str[load].length-1){
-				document.getElementById(`word-${position+1}`).classList.add('active');
-			}
-			position++;
-			console.log(position, str[load].length);
-			if(position==str[load].length){
-				document.getElementById('congratulation').innerText =`Congratulation!!! You have Complete the Test.`;
-			}
-			
-		} else{
-			document.getElementById(`word-${position}`).style.backgroundColor = '#FFC0CB'
-		}
-	} else if(event.key == 'Backspace'){
-		document.getElementById(`word-${position}`).style.backgroundColor = 'white';
-		document.getElementById(`word-${position}`).classList.add('active');
+	// Timer
+	if(timeFlag){
+		setInterval(updateCountDown, 1000);
+		timeFlag = false;
 	}
 	
+	console.log(event.key, position);
+	
+	//If time has finished
+	if(timeHas){
+		if(event.key.length==1){
+			if(event.key == str[load][position]){
+				document.getElementById(`word-${position}`).style.backgroundColor = '#E7FBD3'
+				document.getElementById(`word-${position}`).classList.remove('active');
+				if(position<str[load].length-1){
+					document.getElementById(`word-${position+1}`).classList.add('active');
+				}
+				if(position<str[load].length){
+					position++;
+				}
+				console.log(position, str[load].length);
+				
+				
+			} else{
+				document.getElementById(`word-${position}`).style.backgroundColor = '#FFC0CB'
+				document.getElementById(`word-${position}`).classList.remove('active');
+				if(position<str[load].length-1){
+					document.getElementById(`word-${position+1}`).classList.add('active');
+				}
+				if(position<str[load].length){
+					position++;
+				}
+			}
+		} else if(event.key == 'Backspace'){
+			
+			document.getElementById(`word-${position-1}`).classList.add('active');
+			document.getElementById(`word-${position}`).classList.remove('active');
+			position--;
+			document.getElementById(`word-${position}`).style.backgroundColor = 'white';
+		}
+	} 
+
+
+	if(position==str[load].length){
+		document.getElementById('congratulation').innerText =`Congratulation!!! You have Complete the Test.`;
+		timeDecreaseNeed = false;
+	}
 })
-
-
+const updateCountDown = () => {
+	
+	if(time>0){
+		if(timeDecreaseNeed){
+			time--;
+		}
+		
+	}
+	console.log('Timer :', time);
+	document.getElementById('countDown').innerText = time;
+	if(time==timePos){
+		timePos++;
+		document.getElementById('congratulation').innerText =`Congratulation!!! You have Complete the Test.`;
+		timeHas = false;
+		
+	}
+};
+console.log("sakib");
 // Try again button
 const tryAgain = () => {
 	window.location.reload();
